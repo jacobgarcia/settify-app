9a67e0341384da0f2514399699eb2b8fc0eca60c
jacobgarcia <rock2494@hotmail.com>
2020-03-30 13:08:07 -0600

    Add functionality to user profile

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,10 +1,17 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, View, Image, NativeModules, Text} from 'react-native';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  View,
+  Image,
+  NativeModules,
+  Text,
+} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {AuthSession} from 'expo';
 import {Ionicons} from '@expo/vector-icons';
-import {Container, Header, Content, Thumbnail} from 'native-base';
+import {Container, Header, Content, Thumbnail, Toast, Root} from 'native-base';
 import Playlists from 'views/Playlists';
 import LoginButton from 'components/Button';
 import Logo from 'assets/image.png';
@@ -12,77 +19,113 @@ import useAuth from 'hooks/auth';
 import theme from 'styles/theme.style.js';
 import {Title, Subtitle} from './styled';
 
+import API from 'api';
+
 function SettingsScreen() {
-  const uri = 'https://randomuser.me/api/portraits/men/32.jpg';
+  const [user, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const data = await API.Spotify.GetProfile();
+      setData(data);
+    } catch (error) {
+      Toast.show({
+        text: 'An error occured while getting the playlists',
+        type: 'danger',
+        textStyle: {
+          textAlign: 'center',
+        },
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const uri =
+    'https://www.pngkey.com/png/detail/230-2301779_best-classified-apps-default-user-profile.png';
+
   return (
-    <Container>
-      <Header style={{backgroundColor: theme.COLOR_PRIMARY}} />
-      <Content contentContainerStyle={styles.logo}>
-        <Thumbnail
-          style={{
-            height: 120,
-            width: 120,
-            borderRadius: 120 / 2,
-            marginTop: 40,
-          }}
-          large
-          source={{uri: uri}}
-        />
-        <Text
-          style={{
-            marginTop: 20,
-            fontSize: 25,
-            fontWeight: 'bold',
-            color: '#6e6e6e',
-          }}>
-          Jacob Garcia
-        </Text>
-        <View
-          style={{
-            flexDirection: 'row',
-            marginTop: 100,
-          }}>
-          <Ionicons
-            name="ios-person"
-            style={{
-              fontSize: 35,
-              color: '#6e6e6e',
-              marginRight: 20,
-            }}
-          />
-          <Text
-            style={{
-              marginTop: 4,
-              fontSize: 20,
-              color: '#6e6e6e',
-            }}>
-            fatalraincloud
-          </Text>
+    <Root>
+      {loading ? (
+        <View style={[styles.loadingContainer]}>
+          <ActivityIndicator size="large" color={theme.COLOR_PRIMARY} />
         </View>
-        <View
-          style={{
-            flexDirection: 'row',
-            marginTop: 20,
-          }}>
-          <Ionicons
-            name="ios-mail"
-            style={{
-              fontSize: 35,
-              color: '#6e6e6e',
-              marginRight: 20,
-            }}
-          />
-          <Text
-            style={{
-              fontSize: 20,
-              color: '#6e6e6e',
-              marginTop: 4,
-            }}>
-            rock2494@hotmail.com
-          </Text>
-        </View>
-      </Content>
-    </Container>
+      ) : (
+        <Container>
+          <Header style={{backgroundColor: theme.COLOR_PRIMARY}} />
+          <Content contentContainerStyle={styles.logo}>
+            <Thumbnail
+              style={{
+                height: 120,
+                width: 120,
+                borderRadius: 120 / 2,
+                marginTop: 40,
+              }}
+              large
+              source={{uri: user.images ? user.images[0].url : uri}}
+            />
+            <Text
+              style={{
+                marginTop: 20,
+                fontSize: 25,
+                fontWeight: 'bold',
+                color: '#6e6e6e',
+              }}>
+              {user.display_name}
+            </Text>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 100,
+              }}>
+              <Ionicons
+                name="ios-person"
+                style={{
+                  fontSize: 35,
+                  color: '#6e6e6e',
+                  marginRight: 20,
+                }}
+              />
+              <Text
+                style={{
+                  marginTop: 4,
+                  fontSize: 20,
+                  color: '#6e6e6e',
+                }}>
+                {user.id}
+              </Text>
+            </View>
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: 20,
+              }}>
+              <Ionicons
+                name="ios-mail"
+                style={{
+                  fontSize: 35,
+                  color: '#6e6e6e',
+                  marginRight: 20,
+                }}
+              />
+              <Text
+                style={{
+                  fontSize: 20,
+                  color: '#6e6e6e',
+                  marginTop: 4,
+                }}>
+                {user.email}
+              </Text>
+            </View>
+          </Content>
+        </Container>
+      )}
+    </Root>
   );
 }
 
